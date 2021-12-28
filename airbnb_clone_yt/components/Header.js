8770b1bb4881b5cbab16640c4f013bdc2b2643d8
kanaktyagi <tyagiknk.india@gmail.com>
2021-12-28 22:00:48 +0530

    Added the smallcard component for airbnb

--- airbnb_clone_yt/components/Header.js
+++ airbnb_clone_yt/components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
           </div>
           <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
           <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder="Start your search"/>
-          <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
+              <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
           </div>
           <div className='flex items-center justify-end space-x-4  text-gray-500'>
           <p className='hidden md:inline cursor-pointer'>Become a host</p>
--- airbnb_clone_yt/pages/index.js
+++ airbnb_clone_yt/pages/index.js
@@ -1,15 +1,43 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
+  console.log("exploreData", exploreData);
   return (
     <div>
-    <Head>
-    <title>airbnb</title>
-    </Head>
-    <Header/>
-    <Banner/>
+      <Head>
+        <title>airbnb</title>
+      </Head>
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5"> Explore Nearly</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {exploreData?.map((item) => (
+            <SmallCard
+              key={item.img}
+              img={item.img}
+              distance={item.distance}
+              location={item.location}
+            />
+          ))}
+          </div>
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
