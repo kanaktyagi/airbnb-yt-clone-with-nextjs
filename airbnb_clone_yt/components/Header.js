5607db8fd1592f60e283d19dc20f141b5b78c9ef
kanaktyagi <tyagiknk.india@gmail.com>
2021-12-30 14:14:14 +0530

    Added the cancel and number of guests button ui

--- airbnb_clone_yt/components/Header.js
+++ airbnb_clone_yt/components/Header.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import Image from 'next/image';
-import {SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon,UserIcon} from '@heroicons/react/solid'
+import {SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon,UsersIcon} from '@heroicons/react/solid'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
@@ -9,6 +9,7 @@ export default function Header() {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
 
     const selectionRange = {
         startDate: startDate,
@@ -19,6 +20,9 @@ export default function Header() {
         setStartDate(ranges.selection.startDate);
         setEndDate(ranges.selection.endDate);
 
+    }
+    const resetInput = () => {
+        setSearchInput("")
     }
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3  bg-white shadow-md p-5 md:px-10'>
@@ -50,7 +54,16 @@ export default function Header() {
          minDate={new Date()}
          rangeColors={["#FD5B61"]}
          onChange={handleSelect}
-         /></div>
+         />
+        <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+            <UsersIcon className='h-5'/>
+            <input value={noOfGuests} min={1} onChange={(e) => setNoOfGuests(e.target.value)} type="number" className='w-12 pl-2 text-lg outline-none text-red-400'/>
+        </div>
+        <div className='flex'>
+        <button onClick={resetInput} className='flex-grow text-gray-500'> Cancel </button>
+        <button className='flex-grow  text-red-400'> Search </button> </div>
+         </div>
           }
         </header>
     )
