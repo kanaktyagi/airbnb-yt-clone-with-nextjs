5d3017cc7df99b5feb2212fc1ff5add97426afce
kanaktyagi <tyagiknk.india@gmail.com>
2022-01-01 20:07:04 +0530

    Added the mapbox key in config file

--- airbnb_clone_yt/next.config.js
+++ airbnb_clone_yt/next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['links.papareact.com'],
     },
+    env: {
+      mapbox_key: "pk.eyJ1IjoidHlhZ2lrbmsiLCJhIjoiY2t4dnZqZDFqM3o2eTJ5cWticnZqdWRkOCJ9.TP6xqvGCTX7vZzXsoJGpHA"
+    }
   }
