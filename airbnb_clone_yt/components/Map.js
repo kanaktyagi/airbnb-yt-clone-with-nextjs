3dd4c4fcc5809503422e5149aadc5ec3faf0c851
kanaktyagi <tyagiknk.india@gmail.com>
2022-01-02 17:25:23 +0530

    Added the pin pointer and location description on click of pin

--- airbnb_clone_yt/components/Map.js
+++ airbnb_clone_yt/components/Map.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL,{Marker, Popup} from 'react-map-gl';
+import getCenter from 'geolib/es/getCenter';
 
-function Map() {
-    const [viewport, setViewport] = useState({
+function Map({searchResults}) {
+    const [selectedLocation,setSelectedLocation] = useState({})
+  
+      const coordinates = searchResults.map(result=> ({
+          longitude: result.long,
+          latitude: result.lat
+      }))
+      const  center = getCenter(coordinates)
+
+      const [viewport, setViewport] = useState({
         width: '100%',
         height: '100%',
-        latitude: 37.7577,
-        longitude: -122.4376,
+        latitude: center.latitude ,
+        longitude: center.longitude,
         zoom: 11
       });
     return (
@@ -16,6 +25,33 @@ function Map() {
         {...viewport}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         >
+        {
+            searchResults.map(result => (
+                <div key={result.long}>
+                    <Marker longitude={result.long} latitude={result.lat}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                    >
+                    <p role="img" onClick={()=> setSelectedLocation(result)} aria-label="push-pin" className='cursor-pointer text-2xl animate-bounce'>🏓</p>
+                   {selectedLocation.long === result.long ? ( 
+                    //    <Popup 
+                    //    //onClose={() => setSelectedLocation({})}
+                    //   // closeOnClick={true}
+                    //    latitude={result.lat}
+                    //    longitude={result.long}
+                    //    className='text-gray-100'
+
+                    //    >
+                     <div className='text-gray-800 bg-gray-100 px-2 py-1 rounded-xl'>  {result.title} </div>
+                        //  </Popup>
+               
+                   ): 
+                   null}
+                    </Marker>
+
+                </div>
+            ))
+        }
             
         </ReactMapGL>
     )
